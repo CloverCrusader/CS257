a3992c7258e61e6e50f40965d2ba5d5f0ccf3c83
CloverCrusader <80260043+CloverCrusader@users.noreply.github.com>
2024-04-17 10:46:01 -0500

    I totally forgot how to python

--- pysqlLab/createtables.py
+++ pysqlLab/createtables.py
@@ -1,22 +1,27 @@
 import psycopg2
 
-conn = psycopg2.connect(
-        host="localhost",
-        port=5432,   
-        database="rapaczs",
-        user="rapaczs",
-        password="chip979bond")
-
-if conn is not None:
-        print( "Connection Worked!" )
-else:
-        print( "Problem with Connection" )
+def main():
+        conn = psycopg2.connect(
+                host="localhost",
+                port=5432,   
+                database="rapaczs",
+                user="rapaczs",
+                password="chip979bond")
+        
+        if conn is not None:
+                print( "Connection Worked!" )
+        else:
+                print( "Problem with Connection" )
+                return None
+              
+        cur = conn.cursor()
+        
+        sql = "DROP TABLE IF EXISTS statepopulation; CREATE TABLE statepopulation (code text, statename text, pop real); DROP TABLE IF EXISTS topcities; CREATE TABLE topcities (city text, statename text, citypop real, lat real, lon real);"
+            
+        cur.execute( sql )
+        
         return None
-      
-cur = conn.cursor()
 
-sql = "DROP TABLE IF EXISTS statepopulation; CREATE TABLE statepopulation (code text, statename text, pop real); DROP TABLE IF EXISTS topcities; CREATE TABLE topcities (city text, statename text, citypop real, lat real, lon real);"
-    
-cur.execute( sql )
-
-return None
+# run main
+if __name__ == "__main__":
+        main()
