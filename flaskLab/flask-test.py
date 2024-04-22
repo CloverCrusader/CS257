0dbe1bda653a34156644702a6fe531ea9dfe8415
CloverCrusader <rapaczs@carleton.edu>
2024-04-22 15:44:50 -0500

    first flask sql query

--- flaskLab/flask-test.py
+++ flaskLab/flask-test.py
@@ -16,6 +16,41 @@ def my_display(word1, word2):
 def my_color(word1):
     return '<h1 style="color:Red">' + word1 + '</h1>'
 
+@app.route('/pop/<abbrev>')
+def state_pop(abbrev):
+    conn = psycopg2.connect(
+                host="localhost",
+                port=5432,   
+                database="rapaczs",
+                user="rapaczs",
+                password="chip979bond")
+        
+        if conn is not None:
+                print( "Connection Worked!" )
+        else:
+                print( "Problem with Connection" )
+                conn.close()
+                return None
+              
+        cur = conn.cursor()
+
+        abbrev = abbrev.upper()
+
+        sql = "SELECT pop FROM statepopulation WHERE code = %s;"
+
+        statedata = (state, )
+        cur.execute( sql, statedata )
+
+        if cur.fetchone() is not None:
+            pop = cur.fetchone[0]
+            cur.close()
+            conn.close()
+            return pop
+        else:
+            cur.close()
+            conn.close()
+            return None
+
 if __name__ == '__main__':
     my_port = 5123
     app.run(host = '0.0.0.0', port = my_port) 
